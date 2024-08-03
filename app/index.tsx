6a607df1fb9e96f2923ad13b19f2ae78732fccbe
isurusachinda85 <isurusachintha8@gmail.com>
2024-08-03 23:03:34 +0530

    update

--- app/index.tsx
+++ app/index.tsx
@@ -5,11 +5,14 @@ import { View, Text, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity
 
 const Index = () => {
 
-  const [task, setTask] = useState();
+  const [task, setTask] = useState("");
+  const [taskItems, setTaskItems] = useState([""]);
 
   const handleAddTask = () => {
-    console.log(task);
-
+    if (task.trim().length > 0) {
+      setTaskItems([...taskItems, task]);
+      setTask("");
+    }
   }
 
   return (
@@ -19,16 +22,22 @@ const Index = () => {
         <Text className="font-bold text-2xl ">Today 's tasks</Text>
 
         <View className="pt-5">
-          <Task text={"Task 1"} />
-          <Task text={"Task 2"} />
+          {
+            taskItems.map((item, index) => {
+              return <Task key={index} text={item} />
+            })
+          }
+
         </View>
       </View>
 
       <KeyboardAvoidingView behavior={Platform.OS === "android" ? "padding" : "height"}
         className="absolute bottom-6 w-[100%] flex-row justify-around items-center ">
-        <TextInput placeholder="Write a task" className="bg-white rounded-3xl w-[230px] p-3 border border-gray-300" />
+        <TextInput placeholder="Write a task"
+          className="bg-white rounded-3xl w-[230px] p-3 border border-gray-300"
+          value={task} onChangeText={text => setTask(text)} />
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handleAddTask}>
           <View className="w-[60px] h-[60px] bg-white rounded-full border border-gray-300 items-center justify-center">
             <Text>+</Text>
           </View>
