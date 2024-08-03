6868468efe16dce42b6f9e22d0d72412e55aa225
isurusachinda85 <isurusachintha8@gmail.com>
2024-08-03 21:59:09 +0530

    update

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,17 @@
 
 import Task from "@/components/task";
+import { useState } from "react";
 import { View, Text, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity } from "react-native";
 
 const Index = () => {
+
+  const [task, setTask] = useState();
+
+  const handleAddTask = () => {
+    console.log(task);
+
+  }
+
   return (
     <View className="bg-gray-200 flex-1">
 
@@ -16,11 +25,11 @@ const Index = () => {
       </View>
 
       <KeyboardAvoidingView behavior={Platform.OS === "android" ? "padding" : "height"}
-        className="absolute bottom-6 w-[100%] flex-row justify-between items-center ">
-        <TextInput placeholder="Write a task" className="bg-white rounded-xl w-[230px] p-3 border border-gray-300" />
+        className="absolute bottom-6 w-[100%] flex-row justify-around items-center ">
+        <TextInput placeholder="Write a task" className="bg-white rounded-3xl w-[230px] p-3 border border-gray-300" />
 
         <TouchableOpacity>
-          <View className="w-[60px] h-[60px] bg-white rounded-full">
+          <View className="w-[60px] h-[60px] bg-white rounded-full border border-gray-300 items-center justify-center">
             <Text>+</Text>
           </View>
         </TouchableOpacity>
