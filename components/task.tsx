bf4cc3d073e2d79cf519801f823a7447b4b234e3
isurusachinda85 <isurusachintha8@gmail.com>
2024-08-05 02:41:09 +0530

    updated

--- components/task.tsx
+++ components/task.tsx
@@ -3,15 +3,16 @@ import { View, Text, TouchableOpacity } from 'react-native'
 
 const Task = (props: any) => {
     return (
-        <View className='bg-white p-5 rounded-lg flex flex-row items-center justify-between mb-5 '>
+        <View className='bg-white p-5 rounded-lg flex flex-row items-start mb-5 '>
 
-            <TouchableOpacity className='w-[24px] h-[24px] bg-blue-400 rounded-md mr-2'></TouchableOpacity>
 
-            <View className='flex-row items-center w-[80%]'>
+            <TouchableOpacity className='w-[24px] h-[24px] bg-blue-500 rounded-md mr-4'></TouchableOpacity>
+
+            <View className='flex-row items-center w-[80%] '>
                 <Text className='w-full'>{props.text}</Text>
             </View>
 
-            <View className='w-[12px] h-[12px] border border-blue-500 rounded-full'></View>
+
         </View>
     )
 }
